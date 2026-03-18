a2dc39aa97a5ad49cd010897f9022e671d7617a4
Symbat <s_komekbay@kbtu.kz>
2026-03-18 23:52:56 +0500

    Create NameComparator.java

---
+++ pr4/pr4/model/NameComparator.java
@@ -0,0 +1,11 @@
+package problem4.service;
+
+import problem4.model.Student;
+import java.util.Comparator;
+
+public class NameComparator implements Comparator<Student> {
+    @Override
+    public int compare(Student s1, Student s2) {
+        return s1.getName().compareTo(s2.getName());
+    }
+}
