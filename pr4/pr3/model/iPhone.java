2b76c344cff7654e1eb52a55900daf889b090449
Symbat <s_komekbay@kbtu.kz>
2026-03-18 23:46:15 +0500

    Create iPhone.java

---
+++ pr4/pr3/model/iPhone.java
@@ -0,0 +1,6 @@
+package problem3.model;
+
+public class iPhone implements SellableAndPluggable {
+    public void sell() { System.out.println("iPhone is sold"); }
+    public void plug() { System.out.println("iPhone is plugged"); }
+}
