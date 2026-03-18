de33df1e13a46e79a65d63f081a8b0dcc81d83e2
Symbat <s_komekbay@kbtu.kz>
2026-03-18 23:52:16 +0500

    Create Student.java

---
+++ pr4/pr4/model/Student.java
@@ -0,0 +1,24 @@
+package problem4.model;
+
+public class Student implements Comparable<Student> {
+    private String name;
+    private double gpa;
+    
+    public Student(String name, double gpa) {
+        this.name = name;
+        this.gpa = gpa;
+    }
+    
+    public String getName() { return name; }
+    public double getGpa() { return gpa; }
+    
+    @Override
+    public int compareTo(Student other) {
+        return Double.compare(this.gpa, other.gpa);
+    }
+    
+    @Override
+    public String toString() {
+        return name + " (" + gpa + ")";
+    }
+}
