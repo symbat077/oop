f55e7e8e61faf5e543858e7096d5b148ead6bf59
Symbat <s_komekbay@kbtu.kz>
2026-03-18 23:43:22 +0500

    Create MemoryGame.java

---
+++ pr4/pr3/model/MemoryGame.java
@@ -0,0 +1,8 @@
+package problem3.model;
+
+public class MemoryGame implements IGame {
+    public void a() { System.out.println("MemoryGame.a()"); }
+    public void b() { System.out.println("MemoryGame.b()"); }
+    public void c() { System.out.println("MemoryGame.c()"); }
+    public void d() { System.out.println("MemoryGame.d()"); }
+}
